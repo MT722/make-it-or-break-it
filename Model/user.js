350e4727b3f0aa6e62d01dcc4c6a968a5deaff37
MT722 <85648603+MT722@users.noreply.github.com>
2021-08-29 16:33:50 -0700

    user model

---
+++ Model/user.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeBulkCreate: async (newUserData) => {
+                // newUserData is an array of values to insert
+                for (var idx = 0; idx < newUserData.length; idx++) {
+                    newUserData[idx].dataValues.password = await bcrypt.hash(newUserData[idx].dataValues.password, 10);
+                }
+                return newUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
