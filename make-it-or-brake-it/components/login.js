1b268b3afbabdc6e5b9e6f87c4cf7d21a9ad9b5e
MT722 <85648603+MT722@users.noreply.github.com>
2021-08-30 14:13:05 -0700

    login and sign up pages

---
+++ make-it-or-brake-it/components/login.js
@@ -0,0 +1,18 @@
+return  (
+    <>
+
+<div>
+    <img/>   
+    <section>
+        <form>
+        <h3>Login</h3>
+        <label for="userName">Username:</label>
+        <input type="text" id="userName"/>
+        <label for="passWord">Password:</label>
+        <input type="text" id="passWord"/>
+            <button>Login</button>
+        </form>
+    </section>
+</div>
+</>
+)
